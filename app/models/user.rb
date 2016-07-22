d39753a7f458e82ceb601c6c342850d4a78957f6
yeunuthongminh <yeunuthongminh@gmail.com>
2016-07-22 16:53:15 +0700

    User Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   before_save :downcase_email
-  validates :name, present: true, length: {maximum: 50}
+  validates :name, presence: true, length: {maximum: 50}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, present: true, length: {maximum: 255},
+  validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
 
-  validates :password, present: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}
   private
 
   def downcase_email
